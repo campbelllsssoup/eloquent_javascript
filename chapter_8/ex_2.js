c9252c29bc89b09226198ca7f75bbba5149407df
campbelllssoup <matjocampbell@gmail.com>
2019-11-30 20:03:34 -0800

    Completes Exercise 8-2

--- chapter_8/ex_2.js
+++ chapter_8/ex_2.js
@@ -24,38 +24,53 @@
     function, and then ensures that the box is locked again 
     before returning, regardless of whether the argument 
     function returned normally or threw an exception.
+
+    For extra points, make sure that if you call withBoxUnlocked
+    when the box is already unlocked, the box stays unlocked.
 */
 
 const box = {
-  locked: true,
+  locked: true, // the bonus works as expected when this property is set to false.
   unlock() { this.locked = false; },
   lock() { this.locked = true; },
   _content: [],
   get content() {
     if (this.locked) throw new Error("Locked!");
-    return this._content;
+    return this._content; // allows you to access the _content property
+    // after you call this get method - get methods are called without
+    // parantheses. They manifest themselves much like normal properties.
   }
 }
 
 function withBoxUnlocked(action) {
-  // Your code here.
+  let intiallyLocked = box.locked;
+  if (intiallyLocked === true) {
+    box.unlock();
+    action();
+    box.lock();
+  } else {
+    action();
+  }
 }
 
 withBoxUnlocked(function(){
   box.content.push("gold piece");
 })
 
+let isInitiallyLocked = box.locked;
 try {
+  console.log('Initial locked state:', box.locked);
   withBoxUnlocked(function(){
     throw new Error("Pirates on the horizon! Abort!");
-  });
+  }, box);
 } catch(e) {
   console.log("Error raised:", e);
+} finally {
+  console.log(box.content); // should output ['gold piece']
+  if (isInitiallyLocked === true){
+    box.lock(); // if the box is initially locked, lock it back.
+  }
+  console.log('Is box locked at end?:', box.locked);
 }
-console.log(box.locked); // expect this to output true.
 
-
-/*
-    For extra points, make sure that if you call withBoxUnlocked
-    when the box is already unlocked, the box stays unlocked.
-*/
+// The above logic works.
