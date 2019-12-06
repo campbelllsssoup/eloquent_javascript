5870c41ef4a07cd888d51b4da3c42eb8aa13c4a6
campbelllssoup <matjocampbell@gmail.com>
2019-12-05 20:18:20 -0800

    Completes Exercise 9-1

--- chapter_9/ex_1.js
+++ chapter_9/ex_1.js
@@ -1,4 +1,4 @@
-// Exercise 9-1: Regexp Golf : NOT DONE...
+// Exercise 9-1: Regexp Golf : DONE!
 
 /*
     Code golf is a term used for the game of trying to express
@@ -26,55 +26,98 @@
 
 // 1) WORKS!!!
 
-// let regExpOne = /ca[rt]/;
+let regExpOne = /ca[rt]/;
 
-// let testOne = 'camcapcar';
-// let testTwo = 'campaccat';
-// let testThree = 'campaccal';
+let testOne = 'camcapcar';
+let testTwo = 'campaccat';
+let testThree = 'campaccal';
 
-// console.log(regExpOne.test(testOne)); //=> true
-// console.log(regExpOne.test(testTwo)); //=> true
-// console.log(regExpOne.test(testThree)); //=> false
+console.log(regExpOne.test(testOne)); //=> true
+console.log(regExpOne.test(testTwo)); //=> true
+console.log(regExpOne.test(testThree)); //=> false
 
 // 2) WORKS!!!
 
-// let regExpTwo = /pr?op/;
+let regExpTwo = /pr?op/;
 
-// let testFour = "turbo-prop";
-// let testFive = "push-pop";
-// let testSix = "proppopnotslop";
-// let testSeven = "hellojellopoiproip";
+let testFour = "turbo-prop";
+let testFive = "push-pop";
+let testSix = "proppopnotslop";
+let testSeven = "hellojellopoiproip";
 
-// console.log(regExpTwo.test(testFour));  //=> true
-// console.log(regExpTwo.test(testFive)); //=> true
-// console.log(regExpTwo.test(testSix));  //=> true
-// console.log(regExpTwo.test(testSeven)); //=> false
+console.log(regExpTwo.test(testFour));  //=> true
+console.log(regExpTwo.test(testFive)); //=> true
+console.log(regExpTwo.test(testSix));  //=> true
+console.log(regExpTwo.test(testSeven)); //=> false
 
 // 3) WORKS!!!
 
-// let regExpThree = /ferr(et|y|ari)/;
+let regExpThree = /ferr(et|y|ari)/;
 
-// let testEight = "brand new ferry";
-// let testNine = "sexy ferrari";
-// let testTen = "same ol' ferret";
-// let testEleven = "ferrousferritferroriferri";
+let testEight = "brand new ferry";
+let testNine = "sexy ferrari";
+let testTen = "same ol' ferret";
+let testEleven = "ferrousferritferroriferri";
 
-// console.log(regExpThree.test(testEight)); //=> true
-// console.log(regExpThree.test(testNine)); //=> true
-// console.log(regExpThree.test(testTen)); //=> true
-// console.log(regExpThree.test(testEleven)); //=> false
+console.log(regExpThree.test(testEight)); //=> true
+console.log(regExpThree.test(testNine)); //=> true
+console.log(regExpThree.test(testTen)); //=> true
+console.log(regExpThree.test(testEleven)); //=> false
 
 // 4) WORKS!!!
 
-// let regExpFour = /\b.*(ious)$/;
+let regExpFour = /\b.*(ious)$/;
 
-// let testTwelve = "furious";
-// let testThirteen = "curious";
-// let testFourteen = "superfluous";
+let testTwelve = "furious";
+let testThirteen = "curious";
+let testFourteen = "superfluous";
 
-// console.log(regExpFour.test(testTwelve)); //=> true
-// console.log(regExpFour.test(testThirteen)); //=> true
-// console.log(regExpFour.test(testFourteen)); //=> false
+console.log(regExpFour.test(testTwelve)); //=> true
+console.log(regExpFour.test(testThirteen)); //=> true
+console.log(regExpFour.test(testFourteen)); //=> false
 
-// 5) NOT DONE...
+// 5) WORKS!!!
 
+let regExpFive = /\s[.,:;]/;
+
+let testFifteen = " .avd";
+let testSixteen = "\r.adf";
+let testSeventeen = "\t,asdf";
+let testEighteen = "\v:asdf";
+let testNineteen = "helloooo; !";
+
+console.log(regExpFive.test(testFifteen)); //=> true
+console.log(regExpFive.test(testSixteen)); //=> true
+console.log(regExpFive.test(testSeventeen)); //=> true
+console.log(regExpFive.test(testEighteen)); //=> true
+console.log(regExpFive.test(testNineteen)); //=> false
+
+// 6) WORKS!!!
+
+let regExpSix = /\b\w{6,}/;
+
+let testTwenty = "abc"; 
+let testTwentyOne = "abc a;lsd kfj";
+let testTwentyTwo = "hello abcdef"; 
+let testTwentyThree = "one two three four five seventeen";
+
+console.log(regExpSix.test(testTwenty)); //=> false
+console.log(regExpSix.test(testTwentyOne)); //=> false
+console.log(regExpSix.test(testTwentyTwo)); //=> true
+console.log(regExpSix.test(testTwentyThree)); //=> true
+
+// 7. DONE!!!
+
+let regExpSeven = /\b[^e]+\b$/i;
+
+let testTwentyFour = 'hello';
+let testTwentyFive = 'yoyo';
+let testTwentySix = 'howdy';
+let testTwentySeven = "hello jello howdee";
+let testTwentyEight = "hello jello howdee yall";
+
+console.log(regExpSeven.test(testTwentyFour)); //=> false
+console.log(regExpSeven.test(testTwentyFive)); //=> true
+console.log(regExpSeven.test(testTwentySix)); //=> true
+console.log(regExpSeven.test(testTwentySeven)); //=> false
+console.log(regExpSeven.test(testTwentyEight)); //=> true
