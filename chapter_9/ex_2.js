e7237b68e145fff2017f3a513a2f30c653d9e06c
campbelllssoup <matjocampbell@gmail.com>
2019-12-05 20:57:26 -0800

    Completes Exercise 9-2

--- chapter_9/ex_2.js
+++ chapter_9/ex_2.js
@@ -1,4 +1,4 @@
-// Exercise 9-2: Quoting Style : NOT DONE...
+// Exercise 9-2: Quoting Style : DONE!!!
 
 /*
     Imagine you have written a story and used single quotations marks
@@ -9,3 +9,17 @@
     Think of a pattern that distinguishes these two kinds of quote usage
     and craft a call to the replace method that does the proper replacement.
 */
+
+let text = `It will probably make you feel a bit better about yourself that Tolstoy’s 
+goals didn’t always go to plan. In fact, they rarely did. Some days he kept to his 
+regime and rules, but on others he did 'nothing', 'almost nothing', did things 'badly', 
+'read Gogol' or 'overslept.' However, perhaps without setting his expectations so high 
+he never would have created the literary masterpieces he did. Its'.`;
+
+let replaceSingleQuotes = /'(.*?)'/g;
+
+console.log(text.replace(replaceSingleQuotes, function(match, g1){
+  console.log("------------")
+  return `"${g1}"`;    
+}));
+
