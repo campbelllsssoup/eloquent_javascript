ed41a2ef123f9bb4aac77fd9e74241b11590bbe4
campbelllssoup <matjocampbell@gmail.com>
2020-07-01 18:46:31 -0700

    Refactors ch_4 ex_4

--- chapter_4/ex_4.js
+++ chapter_4/ex_4.js
@@ -15,57 +15,54 @@
 	deep comparison. But you have to take one silly exception into account:
 	because of a historical accident, typeof null also produces "object".
 
-        The Object.keys function will be useful when you need to go over the 
+  The Object.keys function will be useful when you need to go over the 
 	properties of objects to compare them.
-
-        First if checks if the two value types are the same.
-        Second if (else if) checks if the two values types that are object at their base
-        are the same or different (instance of Array is an 'object' type and so is Object, but 
-        both do not have the same properties.)
-
 */
 
-compareKeys = (obj1, obj2) => {
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  } else {
-    for(let i = 0; i < Object.keys(obj1).length; i++){
-      if (Object.keys(obj1)[i] !== Object.keys(obj2)[i]) return false;
+const compareKeys = (obj1, obj2) => {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+  for (let i = 0; i < Object.keys(obj1).length; i++) {
+    if (Object.keys(obj1)[i] !== Object.keys(obj2)[i]) return false;
+  }
+  return true;
+}
+
+const compareValues = (obj1, obj2) => {
+  if (Object.values(obj1).length !== Object.values(obj2).length) return false;
+  for (let i = 0; i < Object.values(obj1).length; i++) {
+    if (Object.values(obj1)[i] !== Object.values(obj2)[i]) return false;
+  }
+  return true;
+}
+
+const deepEqual = (valOne, valTwo) => {
+  if (typeof valOne !== 'object' && typeof valTwo !== 'object') {
+    // return the result of strict comparison,, works becuase these are non-object values.
+    return valOne === valTwo; 
+  } else if (typeof valOne === 'object' && typeof valTwo === 'object') {
+    // catch null values
+    if (valOne === null || valTwo === null) return valOne === valTwo; 
+
+    let oneType = valOne.constructor.name;
+    let twoType = valTwo.constructor.name;
+    // if the two objects weren't created by the same factory function, they're not the same.
+    if (oneType !== twoType) { return false; } 
+    else if (oneType === 'Object') {
+      // use helper functions if both values are true objects.
+      return compareValues(valOne, valTwo) && compareKeys(valOne, valTwo);
+    } else if (oneType === 'Array') {
+      // return false if the two arrays aren't the same legnth.
+      if (valOne.length !== valTwo.length) return false; 
+      // iterate through each index in the array and return false if any values don't match.  
+      for (let i = 0; i < valOne.length; i++) {
+        if (valOne[i] !== valTwo[i]) return false;
+      }
+      return true;
     }
   }
-  return true;
 }
 
-compareValues = (obj1, obj2) => {
-  if (Object.values(obj1).length !== Object.values(obj2).length) {
-    return false;
-  } else {
-    for(let i = 0; i < Object.values(obj1).length; i++){
-      if (Object.values(obj1)[i] !== Object.values(obj2)[i]) return false;
-    }
-  }
-  return true;
-}
-
-deepEqual = (val1, val2) => {
-  if (typeof val1 === typeof val2 && typeof val1 !== 'object') {
-    return val1 === val2 ? true : false;
-  } else if (typeof val1 === typeof val2 && typeof val1 === 'object') {
-    if (val1 === null || val2 === null) return false;
-
-    // currently only comparing objects of the normal object type.
-    // need to also account for arrays.
-
-    if (compareValues(val1,val2) === false || compareKeys(val1,val2) === false) return false;
-  }
-  return true;
-}
-
-value1 = ['Matthew', 98];
-value2 = ['Matthew', 989];
-
-// console.log(Object.values(value1));
-// console.log(Object.values(value2));
-// console.log(Object.values(value1) === Object.values(value2));
-
+// testing...
+const value1 = ['yolo', 'man'];
+const value2 = ['yolo', 'ma'];
 console.log(deepEqual(value1, value2));
