f9a70405921f3693c4f5405f38fc168270f32cda
campbelllssoup <matjocampbell@gmail.com>
2020-07-01 17:32:42 -0700

    Refactors ch_4 ex_2

--- chapter_4/ex_2.js
+++ chapter_4/ex_2.js
@@ -11,68 +11,32 @@
 
         The second, reverseArrayInPlace, does what the reverse method 
         does: it modifies the array given as an argument by reversing
-	its elements. Neither may use the standard reverse method.
+	      its elements. Neither may use the standard reverse method.
 	
 	Thinking back to notes about side effects and pure functions
 	in "Functions and Side Effects" on page 54, which variant do
 	you expect to be useful in more situations? Which one runs
-	faster?
+  faster?
+  
+  NOTE: Assume that 
 */
-
-let argError = new Error('ArguementError: You may only supply an array as an argument.');
-
-arrArg = (arr) => {
-  if (typeof arr !== 'object' || arr['length'] == undefined) {
-    throw argError;
-    return false;
-  }
-  return true;
-}
-
-
 
 reverseArray = (arr) => {
   let arrCopy = [];
-  if (arrArg(arr) === false) {
-    throw argError;
-    return;
-  } else {
-    for(let i = arr.length - 1; i >= 0; i--){
-      arrCopy.push(arr[i]);
-    }
+  for (let i = arr.length - 1; i > -1; i--) {
+    arrCopy.push(arr[i]);
   }
   return arrCopy;
 }
 
-
-
 reverseArrayInPlace = (arr) => {
-  if (arrArg(arr) === false) {
-    throw argError;
-    return;
-  } else {
-    if (arr.length === 1) {
-      return arr;
-    } else {
-      for(let i = 1; i <= Math.floor(arr.length / 2); i++){
-        sub = arr[i-1];
-        arr[i-1] = arr[arr.length - i];
-        arr[arr.length - i] = sub;
-      }
-      return arr;
-    }
+  for (let i = 0; i <= Math.floor(arr.length / 2); i++) {
+    sub = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = sub;
   }
-
+  return arr;
 }
 
-
-
-
-
-try {
-  // console.log(reverseArray([1,2,4,100]));
-  console.log(reverseArrayInPlace(['r','a','c','e','c','a','r','s']));
-
-} catch(e) {
-  console.log(e);
-}
+console.log( reverseArray(['a', 's', 'l', 'u', 'T']) ); //=> [ 'T', 'u', 'l', 's', 'a' ]
+console.log( reverseArrayInPlace(['a', 't', 'n', 'a', 'l', 't', 'A']) ); //=> [ 'A', 't', 'l', 'a', 'n', 't', 'a' ]
