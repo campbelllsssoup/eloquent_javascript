f484a6b76670e2d4095ddfa4d6bbf07fb432f3ab
campbelllssoup <matjocampbell@gmail.com>
2020-07-17 15:25:50 -0700

    Completes Chapter 11 Exercise 2

--- chapter_11/ex_2.js
+++ chapter_11/ex_2.js
@@ -15,31 +15,45 @@
 */
 
 function Promise_all(promises) {
-
+  return new Promise((resolve, reject) => {
+    if (promises.length == 0) { resolve([]) }
+    let counter = promises.length;
+    let result = [];
+    promises.forEach((promise,idx) => {
+      promise
+        .then(value => {
+          result[idx] = value;
+          counter--;
+          if (counter == 0) { 
+            {result, counter}
+            resolve(result) 
+          }
+        })
+        .catch(reject)
+    })
+  })
 }
 
 // Tests
 
-// Promise_all([]).then(array => {
-//   console.log("This should be []:", array);
-// });
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
 
-// function soon(val) {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(val), Math.random() * 500);
-//   });
-// }
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
 
-// Promise_all([soon(1), soon(2), soon(3)]).then(array => {
-//   console.log("This should be [1, 2, 3]:", array);
-// });
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
 
-// Promise_all([soon(1), Promise.reject("X"), soon(3)])
-//   .then(array => {
-//     console.log("We should not get here");
-//   })
-//   .catch(error => {
-//     if (error != "X") {
-//       console.log("Unexpected failure:", error);
-//     }
-//   });
+Promise_all([soon(1), Promise.reject("X"), soon(3)])
+  .then(array => {
+    console.log("We should not get here");
+  })
+  .catch(error => {
+    console.log(`This should read 'X': ${error}`)
+  });
